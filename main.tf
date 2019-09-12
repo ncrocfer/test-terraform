9bc87c8e92734af1bc5584e3aac8ad3e14d24297
Nicolas Crocfer <nicolas.crocfer@corp.ovh.com>
2019-09-12 16:44:08 +0200

    Added new VPS

--- main.tf
+++ main.tf
@@ -13,3 +13,15 @@ resource "openstack_compute_keypair_v2" "mykey" {
   public_key = var.mykey
 }
 
+resource "openstack_compute_instance_v2" "my-instance" {
+  name = "my-instance"
+  provider = "openstack.ovh"
+  image_name = "Ubuntu 18.04"
+  flavor_name = "vps-ssd-1"
+
+  key_pair = "${openstack_compute_keypair_v2.mykey.name}"
+  network {
+    name = "Ext-Net"
+  }
+}
+
