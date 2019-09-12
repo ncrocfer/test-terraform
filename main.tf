80a72091ae2d354d63f4a31a2a7ee9360798a8e3
Nicolas Crocfer <nicolas.crocfer@corp.ovh.com>
2019-09-12 16:35:06 +0200

    Added keypair

--- main.tf
+++ main.tf
@@ -7,3 +7,19 @@ terraform {
     }
   }
 }
+
+provider "openstack" {
+  domain_name = "default"
+  alias = "ovh"
+}
+
+variable "mykey" {
+  type = string
+}
+
+resource "openstack_compute_keypair_v2" "mykey" {
+  provider = "openstack.ovh"
+  name = "mykey"
+  public_key = var.mykey
+}
+
