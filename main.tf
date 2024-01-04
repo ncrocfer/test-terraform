6a4c919648c1b095c4310f6c3cffa553eb89e556
Nicolas Crocfer <nicolas.crocfer@ovhcloud.com>
2024-01-04 13:14:21 +0100

    mon cpmmit

--- main.tf
+++ main.tf
@@ -3,4 +3,5 @@ resource "null_resource" "nicolas" {}
 resource "null_resource" "ismail" {}
 resource "null_resource" "benoit" {}
 resource "null_resource" "arnaud" {}
+resource "null_resource" "maxime" {}
 
