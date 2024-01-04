c48294bc09f8ab3d74eb02944745fc01f9822504
Nicolas Crocfer <nicolas.crocfer@ovhcloud.com>
2024-01-04 13:07:52 +0100

    test

--- main.tf
+++ main.tf
@@ -2,3 +2,5 @@ resource "null_resource" "test" {}
 resource "null_resource" "nicolas" {}
 resource "null_resource" "ismail" {}
 resource "null_resource" "benoit" {}
+resource "null_resource" "arnaud" {}
+
